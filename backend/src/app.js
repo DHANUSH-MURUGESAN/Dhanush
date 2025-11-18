c55b357b41d9257772e66072d3dbcc0e8a219b80
DHANUSH-MURUGESAN <mdhanush102004@gmail.com>
2025-11-19 00:01:46 +0530

    backend update in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,9 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "https://dhanushm-nine.vercel.app/"
+}));
 
 // MongoDB connection
 mongoose
